891acfe56e0bdde33289cb28d0411ec7e3c81e12
Bùi Thiện Tâm <btt28136@gmail.com>
2026-04-06 23:32:19 +0700

    UserController
    Thêm Đăng nhập / Đăng ký + View phân theo Role người dùng

--- obj/Release/net8.0/MTKPM_Clothing_Store_web.AssemblyInfo.cs
+++ obj/Release/net8.0/MTKPM_Clothing_Store_web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTKPM_Clothing_Store_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07a78bac389bb6b3ef0b00320cf5115b58c9f655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee741ed5ce2e52767b8832eccb3385e6bccd2d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTKPM_Clothing_Store_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTKPM_Clothing_Store_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
