5ee9871cc5b39deff412e8071df971dc75604294
Bùi Thiện Tâm <btt28136@gmail.com>
2026-05-26 21:41:37 +0700

    change Dockerfile
    Window container to Linux container

--- obj/Debug/net8.0/MTKPM_Clothing_Store_web.AssemblyInfo.cs
+++ obj/Debug/net8.0/MTKPM_Clothing_Store_web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTKPM_Clothing_Store_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8c7a8494eabccf29a95f6191fb6b1f8db0c38a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce25829a7e7c6887db55698e9c319b4d2570436f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTKPM_Clothing_Store_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTKPM_Clothing_Store_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
