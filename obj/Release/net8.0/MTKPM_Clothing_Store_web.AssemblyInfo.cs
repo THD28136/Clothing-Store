f24f57aad6a695f57e791c5360f35b90519ef5fc
Bùi Thiện Tâm <btt28136@gmail.com>
2026-09-06 02:56:10 +0700

    update
    update appsetting.json

--- obj/Release/net8.0/MTKPM_Clothing_Store_web.AssemblyInfo.cs
+++ obj/Release/net8.0/MTKPM_Clothing_Store_web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTKPM_Clothing_Store_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54697f4f4937df5396edab1b765aa17a70638bc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95e79c4bb054019153807ab6d43eb56394eb1b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTKPM_Clothing_Store_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTKPM_Clothing_Store_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
